20ab9b93abb841fdfd2bcbe3ecd30862e176b3bb
Rohit <vr.rohit03@gmail.com>
2021-04-11 14:58:58 +0530

    mapped components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
     return(
         <div>
             <Header />
-            <Note />
+
+            {notes.map((eachNote) => (
+                <Note
+                    key={eachNote.key}
+                    title={eachNote.title}
+                    content={eachNote.content}
+                />
+            ))}
+
             <Footer />
         </div>
     );
