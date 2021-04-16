c3999536f5edf1eceb5a7252654fec0cd652c056
Rohit <vr.rohit03@gmail.com>
2021-04-16 18:39:04 +0530

    Added onHover event for delete button

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,16 +2,31 @@ import React from "react";
 import DeleteIcon from '@material-ui/icons/Delete';
 
 function Note(props) {
+    const [isHover, setIsHover] = React.useState(false);
 
     function handleDelete() {
         props.onDelete(props.id);
+    }
+
+    function onHoverIn() {
+        setIsHover(true);
+    }
+    function onHoverOut() {
+        setIsHover(false);
     }
 
     return(
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-            <button onClick={handleDelete}><DeleteIcon /></button>
+            <button 
+                onMouseOver={onHoverIn} 
+                onMouseOut={onHoverOut}
+                onClick={handleDelete}
+                style={{ backgroundColor: isHover && "gray"}}
+            >
+                <DeleteIcon />
+            </button>
         </div>
     );
 }
