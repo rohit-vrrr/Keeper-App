6a8b51cf67d5e24ceebb907a216c11d3891312be
Rohit <vr.rohit03@gmail.com>
2021-04-16 18:59:22 +0530

    Updates

---
+++ precache-manifest.ce7ace34da1388e53332a46c592250c6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc41d2152248f78a37f1bfa161dceef4",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "0f061c0524b3b021fc9a",
+    "url": "/Keeper-App/static/js/2.4f2fbe6a.chunk.js"
+  },
+  {
+    "revision": "f379ace7eb8aeb7a1b2d",
+    "url": "/Keeper-App/static/js/main.010c05f3.chunk.js"
+  },
+  {
+    "revision": "450f8c8ae8899abc2755",
+    "url": "/Keeper-App/static/js/runtime-main.d50d7308.js"
+  }
+]);
